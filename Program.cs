085e922c65318510e8d3ebe9e6fe4e562cd34ce8
TS-OTC <ts1026684@otc.edu>
2025-11-11 09:25:19 -0600

    Redo of the entire project
    It is in the Rough Draft

--- Program.cs
+++ Program.cs
@@ -6,187 +6,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(" [Does not save]");
+        int playerHP = 20;
+        int playerD = 1;
         bool redo = false;
         Console.WriteLine("------------------------------------");
         Console.WriteLine("         Adventure Game");
 
         do
         {
-            // Title
             redo = false;
+            // Title
             Console.WriteLine("------------------------------------");
-            Console.WriteLine("1) Instructions");
-            Console.WriteLine("2) Play");
+            Console.WriteLine("1) Play");
+            Console.WriteLine("2) Instructions");
             Console.WriteLine("3) Exit");
             Console.Write(">> ");
             string pick = Console.ReadLine() + "";
-
             switch (pick)
             {
-                case "1": // Going to Instruction
-                    Console.WriteLine("------------------------------------");
-                    direction(redo, pick);
-                    redo = true;
-                    break;
-
-                case "2":  // Going to Play
-                    Console.WriteLine("Welcome to the Adventure Game");
-                    startGame(redo, pick);
-                    break;
-
-                case "3":  // Going to Exit
-                    redo = false;
-                    Console.WriteLine("------------------------------------");
-                    Console.WriteLine("Thanks for playing!");
-                    break;
-
-                default: // Wrong chioce go back to Title
-                    Console.WriteLine("------------------------------------");
-                    redo = true;
-                    Console.WriteLine("THAT WAS NOT AN OPTION!!");
-
-                    break;
-            }
-        } while (redo == true);
-
-
-
-    }
-
-
-
-
-
-
-
-    static void direction(bool redo, string pick)  //instrutions
-    {
-        do
-        {
-            redo = false;
-
-            Console.WriteLine("   Instructions: ");
-            Console.WriteLine("------------------------------------");
-            Console.WriteLine(" Type n to go North");
-            Console.WriteLine(" Type e to go East");
-            Console.WriteLine(" Type s to go South");
-            Console.WriteLine(" Type w to go West");
-            Console.WriteLine(" Type y/n for Yes and No");
-            Console.WriteLine(" Also some will ask for numbers");
-            Console.WriteLine("1) Exit");
-            Console.Write(">> ");
-            pick = Console.ReadLine() + "";
-            if (pick == "1")
-            {
-                return;
-            }
-            else
-            {
-                redo = true;
-                Console.WriteLine("");
-                Console.WriteLine("THAT WAS NOT AN OPTION");
-            }
-        } while (redo == true);
-
-    }
-
-
-
-
-
-
-    static void startGame(bool redo, string pick) // start of the game
-    {
-        string a = " ";
-        string b = " ";
-        string c = " ";
-        string d = " ";
-        string e = " ";
-        string f = " ";
-        string[] inventory = { };    // inventory all items
-        a = "X";
-        do
-        {
-            redo = true;
-            Console.WriteLine("------------------------------------");
-            Console.WriteLine("North of you is a giant steep mountain with a giant door,");
-            Console.WriteLine("south of you is a ragging river, and west and east of you");
-            Console.WriteLine("is all forest.");
-            Console.WriteLine("Where would you like to do?");
-            Console.WriteLine("1) Map");
-            Console.WriteLine("2) Inventory");
-            Console.WriteLine("");
-            Console.Write(">> ");
-            pick = Console.ReadLine() + "";
-
-            switch (pick)
-            {
-                case "1":         // outsideMap
-                    Console.WriteLine("");
-                    outsideMap(redo, a, b, c, d, e, f);
-                    redo = true;
-
+                case "1":
 
                     break;
 
                 case "2":
 
-
-                    Console.WriteLine("");
-                    playerInventory(redo, inventory, pick);
-                    redo = true;
-
                     break;
 
+                case "3":
+
+                    break;
 
                 default:
 
                     break;
             }
+
         } while (redo == true);
+
     }
 
-
-
-
-
-
-    static void playerInventory(bool redo, string[] inventory, string pick) // inventory
-    {
-        foreach (string item in inventory)
-        {
-            Console.WriteLine($" - {item}");
-        }
-        Console.WriteLine("");
-        if (pick == "1")
-        {
-            return;
-        }
-        else
-        {
-            redo = true;
-            Console.WriteLine("");
-            Console.WriteLine("THAT WAS NOT AN OPTION");
-        }
-    }
     
 
 
-    
 
-    static void outsideMap(bool redo, string a, string b, string c, string d, string e, string f) // outside map
+
+    static void player(int playerHP, int playerD)
     {
-        do
-        {
-            Console.WriteLine("---------------");
-            Console.WriteLine($"|{a}|{b}|{c}|{d}|{e}|{f}|");
-            Console.WriteLine("---------------");
 
-        } while (true);
-       
-
-
+        
     }
     
 }
